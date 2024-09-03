97d028d2f008bc2e7fde8c3bc0f99f65c1666277
KoecengDaun <Laurentius.farel@gmail.com>
2024-09-03 19:27:06 +0700

    Memperbaiki Settings

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-vd@pa+)jhw-=md9*paqzg$(%629s5ipr)*z-suw8i2+i1-go&@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "laurentius-farel-mentalhealthtracker.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "laurentius-farel-mentalhealthtracker2.pbp.cs.ui.ac.id"]
 
 
 # Application definition

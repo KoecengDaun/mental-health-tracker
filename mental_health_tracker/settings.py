dbd44c657b010d072d0c4fc04c9dd4f0647487a4
KoecengDaun <Laurentius.farel@gmail.com>
2024-09-11 16:41:09 +0700

    Memperbaiki file yml

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -124,8 +124,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = [
-    "http://localhost",
-    "http://127.0.0.1",
-    "http://laurentius-farel-mentalhealthtracker2.pbp.cs.ui.ac.id",
-    "https://laurentius-farel-mentalhealthtracker2.pbp.cs.ui.ac.id"
+    "http://localhost","http://127.0.0.1","http://laurentius-farel-mentalhealthtracker2.pbp.cs.ui.ac.id","https://laurentius-farel-mentalhealthtracker2.pbp.cs.ui.ac.id"
 ]
